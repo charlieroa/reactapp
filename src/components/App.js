04bde0442db88622ae7e0762caa98097204edc6c
charlieroa <aprendelaa@gmail.com>
2021-07-19 16:29:57 -0500

    Se crea menu con componentes y bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+
+
+const App = () => {
+    return (
+      <>
+         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Header />
+        </nav>
+
+
+      </>
+    );
+}
+ 
+export default App;
