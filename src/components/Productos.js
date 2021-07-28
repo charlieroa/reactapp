d11fbc898adc5dd217f380b8546ac09a262517e2
charlieroa <aprendelaa@gmail.com>
2021-07-28 16:10:25 -0500

    cambio de class a className y entrega del hook useState

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -4,9 +4,9 @@ import Listado from './Listado';
 const Productos = () => {
     return (
         <>
-     <section class="container pt-md-3 pb-5 mb-md-3">
-      <h2 class="h3 text-center">Listado de productos</h2>
-      <div class="row pt-4 mx-n2">
+     <section className="container pt-md-3 pb-5 mb-md-3">
+      <h2 className="h3 text-center">Listado de productos</h2>
+      <div className="row pt-4 mx-n2">
       <Listado    />
     
       </div>   
